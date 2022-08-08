1fa1cec810b702be6b7afb61bc4a17afeb623fa2
moon <xmun74@naver.com>
2022-08-08 18:42:45 +0900

    feat: test edit

--- my-agora-states-client/src/App.test.js
+++ my-agora-states-client/src/App.test.js
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
 import App from "./App";
-import '@testing-library/jest-dom';
 
-it("practice", () => {
-  render(<App />);
-  const nameElement = screen.getByText(/My Agora States/i);
-  expect(nameElement).toBeInTheDocument();
+describe("practice", () => {
+    test('H1 엘리먼트에 텍스트 노드가 있어야 합니다.',()=>{
+        const {container} = render(<App />);
+        const h1 = container.querySelector('h1');
+        expect(h1.textContent).toBe('My Agora States');
+    })
+  
 });
